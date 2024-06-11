bfb051c57e2b885484d07ae9280bfa46128ac2e3
Felipe Miranda Santos <felipe_miranda92@outlook.com>
2024-06-11 17:12:14 -0300

    Iniciando o exercício

---
+++ praticandoRequisicaoAPI/praticandoRequisicaoAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  praticandoRequisicaoAPI
+//
+//  Created by Felipe Miranda Santos on 11/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
