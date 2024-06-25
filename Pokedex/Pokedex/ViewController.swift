01d4ff54517d0714e37b0b05bb94ebd16d4367c5
Felipe Miranda Santos <felipe_miranda92@outlook.com>
2024-06-25 15:35:03 -0300

    Iniciando o projeto

---
+++ Pokedex/Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Felipe Miranda Santos on 25/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
